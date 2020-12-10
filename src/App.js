565f98b784333ddcd72f6a626445cfb5e2e66551
Jay Rajput <74800982+jay-rajput30@users.noreply.github.com>
2020-12-10 19:59:03 +0530

    refactor: used state hooks used useState() to update the buttons and food list

--- src/App.js
+++ src/App.js
@@ -1,73 +1,99 @@
 import React, { useState } from "react";
 import "./styles.css";
-// import data from "./data.js";
+import data from "./data.js";
 
-let indian = {
-  "tandoori chicken": "5/5",
-  samosa: "4/5"
-};
+let foodDataKeys = Object.keys(data);
+// let foodDataValues = Object.values(food);
 
-let chinese = {
-  "manchow soup": "3/5",
-  "fried rice": "4/5"
-};
+// let indian = {
+//   "tandoori chicken": "5/5",
+//   samosa: "4/5"
+// };
 
-let italian = {
-  pasta: "3/5",
-  pizza: "4/5"
-};
+// let chinese = {
+//   "manchow soup": "3/5",
+//   "fried rice": "4/5"
+// };
 
-let american = {
-  hamburger: "4/5",
-  nachos: "3/5"
-};
+// let italian = {
+//   pasta: "3/5",
+//   pizza: "4/5"
+// };
 
-let indianCuisines = Object.keys(indian);
-let indianCuisineRating = Object.values(indian);
+// let american = {
+//   hamburger: "4/5",
+//   nachos: "3/5"
+// };
 
-let chineseCuisines = Object.keys(chinese);
-let chineseCuisineRating = Object.values(chinese);
+// let indianCuisines = Object.keys(indian);
+// let indianCuisineRating = Object.values(indian);
 
-let italianCuisines = Object.keys(italian);
-let italianCuisineRating = Object.values(italian);
+// let chineseCuisines = Object.keys(chinese);
+// let chineseCuisineRating = Object.values(chinese);
 
-let americanCuisines = Object.keys(american);
-let americanCuisineRating = Object.values(american);
+// let italianCuisines = Object.keys(italian);
+// let italianCuisineRating = Object.values(italian);
+
+// let americanCuisines = Object.keys(american);
+// let americanCuisineRating = Object.values(american);
 
 export default function App() {
-  let [item1, setItem1] = useState("food item will appear here");
-  let [item2, setItem2] = useState("food item will appear here");
+  // let [item1, setItem1] = useState("food item will appear here");
+  // let [item2, setItem2] = useState("food item will appear here");
+  let [food, setFood] = useState(data.indian);
+  // let [rating1, setRating1] = useState("");
+  // let [rating2, setRating2] = useState("");
 
-  let [rating1, setRating1] = useState("");
-  let [rating2, setRating2] = useState("");
+  // let [rating, setRating] = useState("");
+  // let [name, setName] = useState("");
 
-  function indianClickHandler() {
-    setItem1(indianCuisines[0]);
-    setItem2(indianCuisines[1]);
-    setRating1(indianCuisineRating[0]);
-    setRating2(indianCuisineRating[1]);
+  // function indianClickHandler() {
+  //   // setItem1(indianCuisines[0]);
+  //   // setItem2(indianCuisines[1]);
+  //   // setRating1(indianCuisineRating[0]);
+  //   // setRating2(indianCuisineRating[1]);
+  //   // foodDataValues.map((x) => {
+  //   //   return console.log(x.name, " ", x.country, " ", x.rating);
+  //   // });
+  // }
+
+  // function chineseClickHandler() {
+  //   setItem1(chineseCuisines[0]);
+  //   setItem2(chineseCuisines[1]);
+  //   setRating1(chineseCuisineRating[0]);
+  //   setRating2(chineseCuisineRating[1]);
+  // }
+
+  // function italianClickHandler() {
+  //   setItem1(italianCuisines[0]);
+  //   setItem2(italianCuisines[1]);
+  //   setRating1(italianCuisineRating[0]);
+  //   setRating2(italianCuisineRating[1]);
+  // }
+  // function americanClickHandler() {
+  //   setItem1(americanCuisines[0]);
+  //   setItem2(americanCuisines[1]);
+  //   setRating1(americanCuisineRating[0]);
+  //   setRating2(americanCuisineRating[1]);
+  // }
+
+  function onClickHandler(country) {
+    setFood(data[country]);
+
+    console.log(food);
   }
 
-  function chineseClickHandler() {
-    setItem1(chineseCuisines[0]);
-    setItem2(chineseCuisines[1]);
-    setRating1(chineseCuisineRating[0]);
-    setRating2(chineseCuisineRating[1]);
-  }
+  function getFoodItem(fItem) {
+    let foodName = fItem.name;
+    let foodRating = fItem.rating;
 
-  function italianClickHandler() {
-    setItem1(italianCuisines[0]);
-    setItem2(italianCuisines[1]);
-    setRating1(italianCuisineRating[0]);
-    setRating2(italianCuisineRating[1]);
+    let listing = (
+      <li className="food-item">
+        {foodName} <strong>{foodRating}</strong>
+      </li>
+    );
+    return listing;
   }
-  function americanClickHandler() {
-    setItem1(americanCuisines[0]);
-    setItem2(americanCuisines[1]);
-    setRating1(americanCuisineRating[0]);
-    setRating2(americanCuisineRating[1]);
-  }
-
   return (
     <div className="App">
       <div className="container">
@@ -79,13 +105,20 @@ export default function App() {
             </span>
           </h1>
           <small>
-            checkout my top 3 food recommendations. Click buttons to view
-            details.
+            checkout my top food recommendations. Click buttons to view details.
           </small>
         </header>
 
         <div className="btnGroup">
-          <button onClick={indianClickHandler} className=" btn btnIndian">
+          {foodDataKeys.map((f) => {
+            return (
+              <button className="btn" onClick={() => onClickHandler(f)}>
+                {f}
+              </button>
+            );
+          })}
+        </div>
+        {/* <button onClick={indianClickHandler} className=" btn btnIndian">
             Indian
           </button>
           <button onClick={chineseClickHandler} className="btn btnChinese">
@@ -96,18 +129,21 @@ export default function App() {
           </button>
           <button onClick={americanClickHandler} className="btn btnThai">
             American
-          </button>
-        </div>
+          </button> */}
       </div>
-
       <ul className="food-list">
+        {food.map((f) => {
+          return getFoodItem(f);
+        })}
+      </ul>
+      {/* <ul className="food-list">
         <li className="food-item">
           {item1} <strong>{rating1}</strong>
         </li>
         <li className="food-item">
           {item2} <strong>{rating2}</strong>
         </li>
-      </ul>
+      </ul> */}
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,21 +1,48 @@
-export default function data() {
-  let indian = {
-    "tandoori chicken": "5/5",
-    samosa: "4/5"
-  };
+let data = {
+  indian: [
+    {
+      name: "tandoori chicken",
+      rating: "5/5"
+    },
+    {
+      name: "samosa",
+      rating: "4/5"
+    }
+  ],
+  chinese: [
+    {
+      name: "tandoori chicken",
+      rating: "3/5"
+    },
+    {
+      name: "fried rice",
+      rating: "4/5"
+    }
+  ]
+};
+//   chinese: {
+//     "manchow soup": "3/5",
+//     "fried rice": "4/5"
+//   }
+// };
 
-  let chinese = {
-    "manchow soup": "3/5",
-    "fried rice": "4/5"
-  };
+// let chinese = {
+//   "manchow soup": "3/5",
+//   "fried rice": "4/5"
+// };
 
-  let italian = {
-    pasta: "3/5",
-    pizza: "4/5"
-  };
+// let italian = {
+//   pasta: "3/5",
+//   pizza: "4/5"
+// };
 
-  let american = {
-    hamburger: "4/5",
-    nachos: "3/5"
-  };
-}
+// let american = {
+//   hamburger: "4/5",
+//   nachos: "3/5"
+// };
+
+// let food = {
+//   [indian]
+// }
+
+export default data;
